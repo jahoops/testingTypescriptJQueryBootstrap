2defc4623bf5f12bce4d781ec64264a7449e30ae
jahoops <jwhooper@gmail.com>
2018-12-13 17:57:12 -0500

    up

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 // rollup -c
+
   // export default {
   //   input: './ts/rollup/report.js',
   //   output: {
@@ -8,11 +9,22 @@
   //     sourcemap: true
   //   }
   // };
+
+// export default {
+//   input: './ts/rollup/jquery_ext_sample.js',
+//   output: {
+//     name: 'reportengine',
+//     file: 'X://reports/js/jquery_ext_sample.js',
+//     format: 'iife',
+//     sourcemap: false
+//   }
+// };
+
 export default {
-  input: './ts/rollup/jquery_ext_sample.js',
+  input: './ts/rollup/jb-flashcards.js',
   output: {
     name: 'reportengine',
-    file: 'X://reports/js/jquery_ext_sample.js',
+    file: './js/jb-flashcards.js',
     format: 'iife',
     sourcemap: false
   }
