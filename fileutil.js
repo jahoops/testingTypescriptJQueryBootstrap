5ce4e4bf0d763d15cdf1763df0a5c248a8e71370
Jahoops <jwhooper@gmail.com>
2018-12-17 22:35:32 -0500

    up

--- fileutil.js
+++ fileutil.js
@@ -1,10 +1,25 @@
-var fs = require("fs");
-var path = "c:\\Temp\\Test.txt";
+function processFile(inputFile) {
+  var fs = require('fs'),
+      readline = require('readline'),
+      instream = fs.createReadStream(inputFile),
+      outstream = new (require('stream'))(),
+      rl = readline.createInterface(instream, outstream),
+      jsonArray = [];
 
-fs.open(path, "w+", function(error, fd) {
-  if (error) {
-    console.error("open error:  " + error.message);
-  } else {
-    console.log("Successfully opened " + path);
-  }
-});
+  rl.on('line', function (line) {    
+      if(line.length===0) return;
+      var qpos = line.indexOf(')');
+      var isnum = line.slice(0,qpos);
+      if(Number.isInteger(Number(isnum))) {
+        jsonArray.push('Q:' + line.slice(qpos+1));
+      } else {
+        jsonArray.push('A:' + line.slice(qpos+1));
+      }
+  });
+  
+  rl.on('close', function (line) {
+     // console.log(line);
+      console.log(jsonArray);
+  });
+}
+processFile('./data/questions.txt');
